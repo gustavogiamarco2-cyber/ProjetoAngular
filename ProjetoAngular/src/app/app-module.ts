6188ea4dde2edcf4674b82d1376f6340301381a2
gustavogiamarco2-cyber <gustavogiamarco2@gmail.com>
2026-08-28 01:45:23 +0000

    novo1

--- ProjetoAngular/src/app/app-module.ts
+++ ProjetoAngular/src/app/app-module.ts
@@ -3,18 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Cadastro } from './Categoria/cadastro/cadastro';
+import { Listagem } from './Categoria/listagem/listagem';
 
 @NgModule({
-  declarations: [
-    App
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-  ],
-  bootstrap: [App]
+  declarations: [App, Cadastro, Listagem],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [provideBrowserGlobalErrorListeners()],
+  bootstrap: [App],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ ProjetoAngular/src/app/cadastro/cadastro-module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CadastroRoutingModule } from './cadastro-routing-module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, CadastroRoutingModule],
+})
+export class CadastroModule {}
